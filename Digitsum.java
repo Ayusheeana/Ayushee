3a232848575cabbc811be51e60d96550c42cdea5
Ayusheeana <2005019@kiit.ac.in>
2022-11-01 20:21:57 +0530

    intial

---
+++ Digitsum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Digitsum {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int n = sc.nextInt();
+        int sum = 0;
+        while (n > 0) {
+            int r = n % 10;
+            sum += r;
+            n /= 10;
+        }
+        System.out.println("The Sum is:" + sum);
+    }
+}
---
+++ Prime.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Prime {
+    static int i = 2;
+
+    public static boolean isPrime(int n) {
+        if (n == 0 || n == 1) {
+            return false;
+        }
+        if (n == i)
+            return true;
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+        return isPrime(n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (isPrime(n)) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+    }
+}
